3e44cb829beb3c4e152d592d5ebd9923b43f2f0b
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-08 18:23:59 -0700

    Printed a C program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
  * main - printing the text
  *
  * Description: using the main function
- * this program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * Return: 1
  */
 int main(void)
