43be3e9690ab28bd2d6196709625aad476639acd
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-12 15:07:36 -0700

    Print a lowercase and uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+ * main - Print an alphabet in uppercase and lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
