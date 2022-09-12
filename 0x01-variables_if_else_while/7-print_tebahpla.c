3cb794cf90d291830b0d02c87a108d4f706b0f1a
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-12 16:22:53 -0700

    Printed alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - print function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
