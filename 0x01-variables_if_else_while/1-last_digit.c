fcbabbb0ec0b2c0d9855b9c187ea96f8ad19b6c9
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-12 13:14:46 -0700

    Printed the last digit in a given variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Print the last digit
  *
@@ -10,7 +11,7 @@
 int main(void)
 {
 	int n; 
-	int x;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
