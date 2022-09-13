ce497b26cf0be896eaeb19f61b549a4e241c8037
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-13 08:26:40 -0700

    Printed the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char x, y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
