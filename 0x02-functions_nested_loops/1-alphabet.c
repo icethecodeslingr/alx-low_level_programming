e2f08c8398e88538615fe5e93fabc92001b3b22e
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-13 05:58:34 -0700

    Printed alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print lowercase alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
