ebeb3680f58d468753b14a4a61506d3eb769779e
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-15 12:48:46 -0700

    Printed a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Print a diagonal
+ * @n: integer used in function
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int len space;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+			{
+				putchar(' ');
+			}
+
+			putchar('\\');
+
+			if (len == (n - 1))
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
