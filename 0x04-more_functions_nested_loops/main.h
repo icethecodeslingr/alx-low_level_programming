fcaa53c287a4a082035714d3c4604deca2819859
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-15 05:14:40 -0700

    Checked for uppercase letters

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * main- header file for prototypes
- * void prototypes (void)
- * int prototypes (int)
- */
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -19,4 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 
-#endif /* MAIN_H */
+#endif
