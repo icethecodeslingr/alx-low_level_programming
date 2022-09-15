46f557128c86515b8349a31159375ba4b5ff0eea
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-15 04:08:15 -0700

    Checked for uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - Check for uppercase letter
+ * @c: the int we will use to argue the function
+ * 
+ * Reurn: 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <== 'Z');
+}
