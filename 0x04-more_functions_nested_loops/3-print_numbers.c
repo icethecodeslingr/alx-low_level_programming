20ce0709500044f8120ab8c490d1ead870ac5694
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-15 06:26:18 -0700

    Printed numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - Print numbers from 0 to 9
- *
  * Return: 0
  */
+
 void print_numbers(void)
 {
 	int x;
 	
 	for (x = 0; x < 10; x++)
-	    _putchar(x + '0');
+		_putchar(x + '0');
 	_putchar('\n');
 }
