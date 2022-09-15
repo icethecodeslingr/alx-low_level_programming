93e63da7932188e713b1f74e602b14b93e26c3e6
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-15 08:58:43 -0700

    Printed 10 times number 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,26 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * more_numbers - printing more numbers
+ * more_numbers - Print numbers
+ *
  * Return: 0
  */
 
 void more_numbers(void)
 {
-	int x, y;
+	int n1, n2;
 
-	for (x = 0; x < 10; x++)
+	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (y = 0; y < 14; y++)
+		for (n2 = 0; n2 <= 14; n2++)
 		{
-			if (y > 9)
+			if (n2 > 9)
 			{
-				_putchar((y / 10) + '0');
+				putchar((n2 / 10) + '0');
 			}
-			_putchar((y % 10) + '0');
+			putchar((n2 % 10) + '0');
 		}
-		_putchar(10);
+		putchar(10);
 	}
 }
