46e5e4e34b6033fd8a335801bc9152881e8383b6
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-15 22:08:17 -0700

    Debugged largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 void print_remaining_days(int, int, int);
-int largest_number(int, int, int);
+int largest_number(int a, int b, int c);
 int convert_day(int, int)
 
 #endif /* MAIN_H */
