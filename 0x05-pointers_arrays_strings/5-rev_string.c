7496259f3ef886ec13b8a49df8e3933c6d214cf9
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-19 11:00:45 -0700

    Reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,21 +9,21 @@
 
 void rev_string(char *s)
 {
-	char s, tmp;
+	chari tmp;
 	int i, length1, length2;
 
 	length1 = 0;
 	length2 = 0;
 
-	while (s(length1) != '\0')
+	while (s[length1] != '\0')
 		length1++;
 
 	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
-		tmp = s(i);
-		s(i) = s(length2);
-		s(length2) = tmp;
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
 		length2 -= 1;
 	}
 }
