1625fba671bf0ea9b073c0b120b2e16233dfded1
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-19 09:29:23 -0700

    Updated the value of int to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of int to 0
+ * @n: int given
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to 98(int *n);
+
+#endif /*MAIN_H*/
