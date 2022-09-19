4ee67b32ef95211c40990b03576f3780acafbfed
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-19 09:51:44 -0700

    Returned the value of length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string
+ * @s: string
+ * Return: 0 
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
