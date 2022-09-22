817d7281eec9dd4d625aaf9bb06513c6fbdf7571
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-09-22 12:53:55 -0700

    Compared two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,10 @@
-#include "main.h"
+#include "main"
 
 /**
- *  _strcmp - function that compares two strings
+ * _strcmp - function that compares string
  * @s1: first string
  * @s2: second string
- * Return:
- * 		returns zero if s1 == s2
- * 		returns negative number if s1 < s2
- * 		returns positive number if s1 > s2
+ * Return: 0
  */
 
 int _strcmp(char *s1, char *s2)
