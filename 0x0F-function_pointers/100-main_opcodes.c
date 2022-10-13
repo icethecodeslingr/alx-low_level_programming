01b795f140ef99954aac8baad5ee15363258da9f
Isaac Igweonu <isaacifyrpn1738@gmail.com>
2022-10-12 17:59:10 -0700

    a program that prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcodes of a given machine
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (count = 0; count < bytes; count++)
+	{
+		printf("%02hhx", *((char *)main + count));
+		if (count < bytes - 1)
+			print(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
